229f55bd35d73bff2a92aa785598a2c6d8a8a90b
Ahmadsultan395 <145713175+Ahmadsultan395@users.noreply.github.com>
2024-10-04 23:07:31 +0500

    setting

--- src/Pages/LandingPage/Journery/Journey.jsx
+++ src/Pages/LandingPage/Journery/Journey.jsx
@@ -30,29 +30,29 @@ function Journey() {
         <section className="hero-sectionj">
           
           <div className="">
-            <img src="/journey.avif" alt="" />
+            <img src="/journey.avif" alt="" width={'100%'}/>
           </div>
           <div className="hero-section-textj">
             <Typography
-              sx={{ color: "#0c111e", fontSize: "4rem", fontWeight: "600", lineHeight: "1.1" }}
+              sx={{ color: "#0c111e", fontSize: "3.5rem", fontWeight: "600", lineHeight: "1.1" , width:'100%' }}
             >
             Take Full Control of Your Guest Journey
             </Typography>
 
-            <Typography sx={{ color: "#0c111e", fontSize: "1.2rem" }}>
+            <Typography sx={{ color: "#0c111e", fontSize: "1.2rem", width:'100%' }}>
             With our guest experience software, you can digitize and automate all guest interactions beyond the booking. The app becomes your guests’ go-to-guide and their digital concierge that supports them during their stay.
             </Typography>
             <br />
-            <Box sx={{ width:'50%'}}>
+            <Box sx={{display: 'flex', width:'100%'}}>
             <Button
             sx={{
               color: "white",
               border:'1px solid #0c111e',
               backgroundColor:'#0c111e',
               textTransform: "none",
-              fontSize: "0.9rem",
+              fontSize: "1rem",
               borderRadius:'50px',
-              padding:'0.5rem 1rem',
+              padding:'0.8rem 1.5rem',
               mr:'2rem',
               '&:hover':{
                 color: "#0c111e",
@@ -69,9 +69,9 @@ function Journey() {
               border:'1px solid #0c111e',
               backgroundColor:'transparent',
               textTransform: "none",
-              fontSize: "0.9rem",
+              fontSize: "1rem",
               borderRadius:'50px',
-              padding:'0.5rem 1rem',
+              padding:'0.8rem 1.5rem',
               '&:hover':{
                 color: "#0c111e",
                 border:'1px solid #0c111e',
